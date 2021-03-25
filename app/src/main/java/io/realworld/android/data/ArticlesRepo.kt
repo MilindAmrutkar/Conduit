19037c4d90f4e93c8e4a57bf51df1c7a4d3874de
MilindAmrutkar <milind27.amrutkar>
2021-03-25 20:10:13 +0530

    Added: RecyclerView and Adapter and FeedFragment

---
+++ app/src/main/java/io/realworld/android/data/ArticlesRepo.kt
@@ -0,0 +1,8 @@
+package io.realworld.android.data
+
+import io.realworld.api.ConduitClient
+
+object ArticlesRepo {
+    val api = ConduitClient().api
+    suspend fun getGlobalFeed() = api.getArticles()
+}
---
+++ app/src/main/java/io/realworld/android/ui/feed/GlobalFeedFragment.kt
@@ -0,0 +1,39 @@
+package io.realworld.android.ui.feed
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.realworld.android.databinding.FragmentFeedBinding
+
+class GlobalFeedFragment: Fragment() {
+
+    private var _binding: FragmentFeedBinding? = null
+    private lateinit var _viewModel : FeedViewModel
+    private lateinit var feedAdapter: ArticleFeedAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _viewModel = ViewModelProvider(this).get(FeedViewModel::class.java)
+        feedAdapter = ArticleFeedAdapter()
+        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        _binding?.feedRecyclerView?.layoutManager = LinearLayoutManager(context)
+        _binding?.feedRecyclerView?.adapter = feedAdapter
+        _viewModel.fetchGlobalFeed()
+        _viewModel.feed.observe({lifecycle}) {
+            feedAdapter.submitList(it)
+        }
+        return _binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
